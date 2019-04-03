2e2b5cae7e881fc4137a71fdf020975b4951b42e
Larissa Cristina <leurissy@mail.uft.edu.br>
2019-04-03 15:41:53 -0300

    correcao model versao app

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -43,11 +43,11 @@ export class LoginPage {
       }
     ).catch(error => console.log(error));
     this.versionNumber = Number(this.versionNumber);
-    this.lastVersion = Number(this.versaoApp_service.getVersaoApp());
+    this.lastVersion = Number(this.versaoApp_service.getVersaoApp);
+    console.log(this.versaoApp_service.getVersaoApp);
     if (this.versionNumber<this.lastVersion){
         this.ferramenta.showAlert("Aplicativo Desatualizado", "O aplicativo não possui a versão minima para uso, por favor atualize seu aplicativo");
         this.blockApp = true;
-      }
     }else{
       this.ferramenta.showAlert("Aplicativo Atualizado", "Aplicativo atualizado!");
     }
