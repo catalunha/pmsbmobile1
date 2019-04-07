7913d70018cebfa2a9ad33e3c80ceac1b450d0be
Kleyson Morais <kleysondev@gmail.com>
2019-04-07 13:37:12 -0300

    Correcao do Bloqueio de Versao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,86 +2,23 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/login/login';
-
-import { AuthenticationServiceLocal } from './../providers/dataLocal/authentication.service';
-import { VersaoAppService } from '../providers/dataServer/versao_app.service';
-import { FerramentasProvider } from '../providers/ferramentas/ferramentas';
-import { AppVersion } from '@ionic-native/app-version';
 import { AtualizacaoPage } from '../pages/atualizacao/atualizacao';
 
 @Component({
   templateUrl: 'app.html',
-  providers: [AuthenticationServiceLocal]
 })
 export class MyApp {
   rootPage: any;
 
   constructor(platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    private authenticarion_local: AuthenticationServiceLocal,
-    private ferramenta: FerramentasProvider,
-    private appVersion: AppVersion,
-    private versaoApp_service: VersaoAppService) {
+    splashScreen: SplashScreen) {
 
-    let usuario_atual = this.authenticarion_local.getAuthentication();
-    let versionVerification = this.appVersionVerification();
-    if(versionVerification){
-      if (usuario_atual) {
-        this.rootPage = TabsPage;
-      } else {
-        this.rootPage = LoginPage;
-      }
-    }else{
-      this.rootPage = AtualizacaoPage;
-    }
-      
-
+    this.rootPage = AtualizacaoPage;
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 
-  public async appAtualizationVerification(){
-    let lastVersion = await this.appLastVersionVerification();
-    let appVersion = await this.appVersionVerification();
-
-    if(lastVersion!==appVersion){
-      return false;
-    }else{
-      return true;
-    }
-  }
-
-  
-  public appVersionVerification() {
-    return new Promise((resolve, reject) => {
-      this.appVersion.getVersionNumber().then(
-      versao => {
-        resolve(versao);
-      }
-      ).catch(error => {
-        this.ferramenta.showAlert("Falha na Verificação!", "Não foi possivel verificar a versão atual do aplicativo, por favor atualize!");
-        this.rootPage = AtualizacaoPage;
-        reject(error);
-      });
-    });
-  }
-  public appLastVersionVerification(){
-    return new Promise((resolve, reject) => {
-      this.versaoApp_service.getVersaoApp({}).subscribe(
-        resposta => {
-          resolve(resposta);
-      },
-      error => {
-        this.ferramenta.showAlert("Falha na Verificação!", "Não foi possivel verificar a versão atual do aplicativo, por favor atualize!");
-        this.rootPage = AtualizacaoPage;
-        reject(error);
-      });
-    });
-  }
 }
--- src/pages/atualizacao/atualizacao.ts
+++ src/pages/atualizacao/atualizacao.ts
@@ -1,25 +1,79 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AtualizacaoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AuthenticationServiceLocal } from '../../providers/dataLocal/authentication.service';
+import { TabsPage } from '../tabs/tabs';
+import { LoginPage } from '../login/login';
+import { FerramentasProvider } from '../../providers/ferramentas/ferramentas';
+import { AppVersion } from '@ionic-native/app-version';
+import { VersaoAppService } from '../../providers/dataServer/versao_app.service';
 
 @IonicPage()
 @Component({
   selector: 'page-atualizacao',
   templateUrl: 'atualizacao.html',
+  providers: [AuthenticationServiceLocal]
 })
 export class AtualizacaoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  block = false;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private authenticarion_local: AuthenticationServiceLocal,
+    private ferramenta: FerramentasProvider,
+    private appVersion: AppVersion,
+    private versaoApp_service: VersaoAppService) {
+
+    this.appAtualizationVerification().then(
+      versionVerification => {
+        if (versionVerification) {
+          console.log("Block False");
+          let usuario_atual = this.authenticarion_local.getAuthentication();
+          if (usuario_atual) this.navCtrl.setRoot(TabsPage);
+          else this.navCtrl.setRoot(LoginPage);
+        } else {
+          console.log("Block True");
+          this.block = true;
+        }
+      }
+    );
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AtualizacaoPage');
+  public async appAtualizationVerification(): Promise<any> {
+    return new Promise(async (resolve) => {
+      let appVersion = await this.appVersionVerification();
+      let lastVersion = await this.appLastVersionVerification();
+      console.log(appVersion);
+      console.log(lastVersion);
+      resolve(lastVersion === appVersion);
+    });
   }
 
+  public appVersionVerification() {
+    return new Promise((resolve) => {
+      this.appVersion.getVersionNumber().then(
+        versao => {
+          resolve(versao);
+        }
+      ).catch(error => {
+        console.log(error);
+        this.ferramenta.showAlert("Falha na Verificação!", "Não foi possivel verificar a versão atual do aplicativo, por favor atualize!");
+        resolve("");
+      });
+    });
+  }
+
+  public appLastVersionVerification() {
+    return new Promise((resolve) => {
+      this.versaoApp_service.getVersaoApp({}).subscribe(
+        resposta => {
+          resolve(resposta);
+        },
+        error => {
+          console.log(error);
+          this.ferramenta.showAlert("Falha na Verificação!", "Não foi possivel verificar a versão atual do disponível, por favor atualize!");
+          resolve("");
+        });
+    });
+  }
 }
